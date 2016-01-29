ef2c70f714b9ddd327f0d1f70720ae54af7b68cf
lecharles <charleslozano@gmail.com>
2016-01-29 04:31:41 -0500

    Generate products controller views: wip. root route: not working.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'poducts#index' #pourquoi ça ne marche pas?
+  resources :products
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
